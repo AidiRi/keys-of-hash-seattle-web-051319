8a402c4f35cd1938d497767baee0a87ea4a0b4ae
AidiRi <rivera.aidi@gmail.com>
2019-05-15 17:05:37 -0700

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,34 @@
+require "pry"
 class Hash
-  def keys_of(arguments)
+
+  def keys_of(*arguments)
     # code goes here
+    array_with_matching_keys = []
+arguments.each do |argument|
+
+    self.select do |key, value|
+      if value == argument
+        array_with_matching_keys << key
+      end
+    # binding.pry
+    end
+end
+     #  self.each do |key, value|
+     #    # arguments.each do |argument|
+     #    #if self value == arguments, return self value
+     #       binding.pry
+     #    if self[value] == argument
+     #       array_with_matching_keys << self[key]
+     #
+     #     end
+     #   # end
+     # end
+
+    return array_with_matching_keys
+    # This method should take an undefined number of
+    # arguments, using the splat operator, and return an
+    # array with every key from the hash whose value matches
+    #  the value(s) given as an argument.
   end
+
 end
